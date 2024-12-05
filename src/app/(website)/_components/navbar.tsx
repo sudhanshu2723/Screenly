9d61210514412d84099f82053d5efc306d3c4bb5
sudhanshu2723 <sudhanshu2723@gmail.com>
2024-12-05 23:20:39 +0530

    error in the file

---
+++ src/app/(website)/_components/navbar.tsx
@@ -0,0 +1,15 @@
+import { Menu } from "lucide-react";
+import Image from "next/image";
+
+
+export default function LandingPageNavbar(){
+    return (
+        <div className="flex w-full justify-between items-center">
+            <div className="text-3xl font-semibold flex items-center gap-x-3">
+                <Menu className="w-6 h-6"/>
+
+            </div>
+        LandingPage Navbar
+        </div>
+    )
+}
